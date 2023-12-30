59e6dbcd238c1ab2d982511f1a5327bd702f3bd6
Cleilton Rocha <cleiltonrv30@gmail.com>
2023-12-30 02:26:12 -0300

    feat: adjust in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 import { AnimatedTitles } from './AnimatedTitles'
 
-import { LinkedinPlainIcon } from 'react-devicons'
-import { GithubIcon, InstagramIcon } from 'lucide-react'
+import { GithubIcon, Linkedin, Dribbble } from 'lucide-react'
 
 import profileRounded from '@/assets/profile-rounded.png'
+import { InfiniteScroll } from './InfiniteScroll'
 
 export function HeroSection() {
   useEffect(() => {
@@ -23,7 +23,8 @@ export function HeroSection() {
   }, [])
 
   return (
-    <section className="bg-[url('../assets/bg-hero.svg')] bg-cover mobile:pb-16 h-screen w-full flex flex-col desktop:items-center mobile:items-center justify-center desktop:px-[100px] mobile:px-4 relative">
+    <section className="mobile:pb-16 h-screen w-full flex flex-col desktop:items-center mobile:items-center justify-center desktop:px-10 mobile:px-4 relative">
+      <div className="absolute top-0 z-[-2] h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="flex items-center justify-center desktop:gap-6 mobile:gap-8 relative">
         <div className="flex flex-col desktop:items-center mobile:items-center mobile:gap-2 desktop:gap-2 ">
           <Image
@@ -34,27 +35,28 @@ export function HeroSection() {
             height={80}
           />
           <AnimatedTitles className="desktop:text-center mobile:text-center">
-            <span className="">UI Designer</span> &<br />{' '}
-            <span className="bg-gradient-to-r leading-tight from-orange-300 via-purple-300 to-purple-500 inline-block text-transparent bg-clip-text">
-              Front-end
-            </span>{' '}
-            Developer
+            <span className="">UI Designer</span> &<br />
+            <span className="inline-flex bg-gradient-to-r from-neutral-900 via-slate-500 to-neutral-500 bg-[200%_auto] bg-clip-text leading-tight text-transparent dark:from-neutral-100 dark:via-slate-400 dark:to-neutral-400">
+              Front-end Developer
+            </span>
           </AnimatedTitles>
-          <p className="paragraph-animated desktop:text-center mobile:text-center font-normal text-gray-400 mobile:max-w-[90%] mobile:text-xl desktop:max-w-[60%] desktop:text-2xl">
+          <p className="mt-6 paragraph-animated desktop:text-center mobile:text-center mobile:max-w-[90%] text-xl desktop:max-w-[60%] text-gray-300">
             Olá! Sou Cleilton Rocha! Eu transformo ideias em interfaces
             intuitivas e crio experiências de usuário incríveis.
           </p>
         </div>
       </div>
-      <div className="mt-10 flex items-start justify-start gap-3">
-        <a
-          href="https://www.instagram.com/uidesign.cr/"
-          aria-label="Link para Instagram"
+      <div className="mt-16 flex items-start justify-start gap-4">
+        {/* <a
+          href="https://www.linkedin.com/in/cleilton-rocha/"
+          aria-label="Link para Linkedin"
           target="_blank"
           rel="noreferrer"
         >
-          <div className="flex h-14 w-14 items-center justify-center rounded-full bg-zinc-50/5 transition-all duration-300 hover:scale-110">
-            <InstagramIcon size="1.5rem" color="#ffffff" />
+          <div className="w-fit h-fit border border-transparent bg-gradient-to-b from-gray-600 to-gray-600/30 bg-origin-border overflow-hidden rounded-full">
+            <div className="flex items-center justify-center bg-gradient-to-b from-neutral-950 to-gray-950/40 h-[60px] w-[60px]">
+              <Linkedin size={24} className="text-slate-300" />
+            </div>
           </div>
         </a>
         <a
@@ -63,22 +65,26 @@ export function HeroSection() {
           target="_blank"
           rel="noreferrer"
         >
-          <div className="flex h-14 w-14 items-center justify-center rounded-full bg-zinc-50/5 transition-all duration-300 hover:scale-110">
-            <LinkedinPlainIcon size="1.5rem" color="#ffffff" />
+          <div className="w-fit h-fit border border-transparent bg-gradient-to-b from-gray-600 to-gray-600/30 bg-origin-border overflow-hidden rounded-full">
+            <div className="flex items-center justify-center bg-gradient-to-b from-neutral-950 to-gray-950/40 h-[60px] w-[60px]">
+              <GithubIcon size={24} className="text-slate-300" />
+            </div>
           </div>
         </a>
         <a
-          href="https://github.com/CleiltonRocha"
-          aria-label="Link para Github"
+          href="https://www.linkedin.com/in/cleilton-rocha/"
+          aria-label="Link para Linkedin"
           target="_blank"
           rel="noreferrer"
         >
-          <div className="flex h-14 w-14 items-center justify-center rounded-full bg-zinc-50/5 transition-all duration-300 hover:scale-110">
-            <GithubIcon size="1.5rem" color="#ffffff" />
+          <div className="w-fit h-fit border border-transparent bg-gradient-to-b from-gray-600 to-gray-600/30 bg-origin-border overflow-hidden rounded-full">
+            <div className="flex items-center justify-center bg-gradient-to-b from-neutral-950 to-gray-950/40 h-[60px] w-[60px]">
+              <Dribbble size={24} className="text-slate-300" />
+            </div>
           </div>
-        </a>
+        </a> */}
       </div>
-      <div className="w-full h-[100px] bg-gradient-to-t from-black via-black/30 to-transparent absolute bottom-0"></div>
+      <InfiniteScroll />
     </section>
   )
 }
